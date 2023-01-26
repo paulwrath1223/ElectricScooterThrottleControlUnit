05bc9375a80ef7f9892f8726462da6b6e1797c6e
Paul Fornage <36117326+paulwrath1223@users.noreply.github.com>
2023-01-26 12:27:24 -0800

    added serial outputs and tag to reverse inputs

--- scooter_TCB.ino
+++ scooter_TCB.ino
@@ -8,6 +8,11 @@
 #define brakeInPin 3
 #define throttleOutputPin 4
 
+#define reverseThrottle false
+#define reverseBrake false
+
+#define serialPrintStatements true
+
 #define throttleLowerEEPROMAddress 0 // addresses are in chunks of 2 bytes
 #define throttleUpperEEPROMAddress 2
 #define brakeLowerEEPROMAddress 4
@@ -29,10 +34,13 @@ int currentThrottleLower;
 int currentBrakeUpper;
 int currentBrakeLower;
 
+int tempOutput;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  if serialPrintStatements{
+    Serial.begin(115200);
+  }
   Servo output;
   output.attach(throttleOutputPin);
   // initialize EEPROM with predefined size
@@ -46,10 +54,23 @@ void setup() {
   currentThrottleLower = read2bytes(throttleLowerEEPROMAddress);
   currentBrakeUpper = read2bytes(brakeUpperEEPROMAddress);
   currentBrakeLower = read2bytes(brakeLowerEEPROMAddress);
+  if serialPrintStatements{
+    Serial.print("currentThrottleUpper as read from EEPROM: ");
+    Serial.println(currentThrottleUpper);
+    Serial.print("currentThrottleLower as read from EEPROM: ");
+    Serial.println(currentThrottleLower);
+    Serial.print("currentBrakeUpper as read from EEPROM: ");
+    Serial.println(currentBrakeUpper);
+    Serial.print("currentBrakeLower as read from EEPROM: ");
+    Serial.println(currentBrakeLower);
+  }
 }
 
 void loop() {
   if(!digitalRead(calSwitchPin)){    //TODO: do I need a 'not'?
+    if serialPrintStatements{
+      Serial.println("Starting calibration mode loop");
+    }
     calibrationMode = true;
     newConfigQueued = false;
     output_to_esc(0);
@@ -94,8 +115,8 @@ void loop() {
 
 void IRAM_ATTR isr(){  // TODO: attach to commit pin interrupt
   if(digitalRead(commitButtonPin)){
-    if(!calibrationMode){
-      if((analogRead(throttleInPin) < potentialNewThrottleLower) && (analogRead(brakeInPin) < potentialNewBrakeLower)){ //   TODO: make sure both inputs read 0
+    if(!calibrationMode && newConfigQueued){
+      if bothInputsZero(){ //   TODO: make sure both inputs read 0
         currentThrottleUpper = potentialNewThrottleUpper;
         currentThrottleLower = potentialNewThrottleLower;
         currentBrakeUpper = potentialNewBrakeUpper;
@@ -105,6 +126,16 @@ void IRAM_ATTR isr(){  // TODO: attach to commit pin interrupt
         write2bytes(throttleUpperEEPROMAddress, currentThrottleUpper);
         write2bytes(brakeLowerEEPROMAddress, currentBrakeLower);
         write2bytes(brakeUpperEEPROMAddress, currentBrakeLower);
+        if serialPrintStatements{
+          Serial.print("currentThrottleUpper written to EEPROM: ");
+          Serial.println(currentThrottleUpper);
+          Serial.print("currentThrottleLower written to EEPROM: ");
+          Serial.println(currentThrottleLower);
+          Serial.print("currentBrakeUpper written to EEPROM: ");
+          Serial.println(currentBrakeUpper);
+          Serial.print("currentBrakeLower written to EEPROM: ");
+          Serial.println(currentBrakeLower);
+        }
       }
     }
   }
@@ -124,14 +155,35 @@ void write2bytes(int addressToWrite, int value){
 }
 
 void output_to_esc(int value){
-  output.write(map(constrain(value, -127, 127), -127, 127, 0, 180));
+  tempOutput = map(constrain(value, -127, 127), -127, 127, 0, 180);
+  if serialPrintStatements{
+    Serial.print("new output: ");
+    Serial.println(tempOutput);
+  }
+  output.write(tempOutput);
 }
 
 int calculate_output(int throttleIn, int BrakeIn){
+  if throttleReverse && brakeReverse{
+    return(map(throttleIn, currentThrottleLower, currentThrottleUpper, 127, 0) - map(brakeIn, currentBrakeLower, currentBrakeUpper, 127, 0));
+  } else if throttleReverse && !brakeReverse{
+    return(map(throttleIn, currentThrottleLower, currentThrottleUpper, 127, 0) - map(brakeIn, currentBrakeLower, currentBrakeUpper, 0, 127));
+  } else if !throttleReverse && brakeReverse{
+    return(map(throttleIn, currentThrottleLower, currentThrottleUpper, 0, 129) - map(brakeIn, currentBrakeLower, currentBrakeUpper, 127, 0));
+  } 
   return(map(throttleIn, currentThrottleLower, currentThrottleUpper, 0, 127) - map(brakeIn, currentBrakeLower, currentBrakeUpper, 0, 127));
 }
 
-
+bool bothInputsZero(){
+  if throttleReverse && brakeReverse{
+    return(analogRead(throttleInPin) > potentialNewThrottleUpper) && (analogRead(brakeInPin) > potentialNewBrakeUpper);
+  } else if throttleReverse && !brakeReverse{
+    return(analogRead(throttleInPin) > potentialNewThrottleUpper) && (analogRead(brakeInPin) < potentialNewBrakeLower);
+  } else if !throttleReverse && brakeReverse{
+    return(analogRead(throttleInPin) < potentialNewThrottleLower) && (analogRead(brakeInPin) > potentialNewBrakeUpper);
+  } 
+  return(analogRead(throttleInPin) < potentialNewThrottleLower) && (analogRead(brakeInPin) < potentialNewBrakeLower);
+}
 
 
 
